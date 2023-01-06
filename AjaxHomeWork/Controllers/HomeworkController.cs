990d92cabbad93b3d0462002f02c10ddef44c5b3
cn11907 <cn11907@gmail.com>
2023-01-06 16:34:46 +0800

    00

--- AjaxHomeWork/Controllers/HomeworkController.cs
+++ AjaxHomeWork/Controllers/HomeworkController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AjaxHomeWork.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AjaxHomeWork.Controllers
 {
     public class HomeworkController : Controller
     {
+        private readonly ILogger<HomeworkController> _logger;
+        private readonly DemoContext _context;
+        public HomeworkController(ILogger<HomeworkController> logger, DemoContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+
         public IActionResult TravelCard()
         {
             return View();
         }
+
+        public IActionResult CheckName()
+        {
+
+            return View();
+        }
+
     }
 }
