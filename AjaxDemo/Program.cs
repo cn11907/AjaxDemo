414ec5d3321b0b8fd8752407781f71fea3143aa3
cn11907 <117073362+cn11907@users.noreply.github.com>
2023-01-15 22:23:09 +0800

    新增上傳Azure

--- AjaxDemo/Program.cs
+++ AjaxDemo/Program.cs
@@ -16,13 +16,22 @@ builder.Services.AddDbContext<DemoContext>(
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+
+// Configure the HTTP request pipeline.¼Ò½k¿ù»~°T®§
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+//¸Ô²Ó¿ù»~°T®§
+//app.UseDeveloperExceptionPage();
+   
+
+
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
